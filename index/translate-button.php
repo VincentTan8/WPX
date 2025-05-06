360e2f29dd9ac793ef748298a792e9f80887fb77
VincentTan8 <vincent.tan@wetalk.com>
2025-05-06 13:49:00 +0800

    feat: updated translate button php to target new flag assets

--- index/translate-button.php
+++ index/translate-button.php
@@ -1,7 +1,10 @@
-<div id="translate-container">
-    <button id="translate-btn">Translate</button>
-    <div id="language-options">
-        <button data-lang="en">English</button>
-        <button data-lang="cn">中文</button>
-    </div>
+<div id="language-options">
+    <button data-lang="en">
+        <img src="../resources/img/en.png" alt="English Flag" style="width: 20px; height: 20px; margin-right: 8px;">
+        EN
+    </button>
+    <button data-lang="cn">
+        <img src="../resources/img/cn.png" alt="Chinese Flag" style="width: 20px; height: 20px; margin-right: 8px;">
+        中文
+    </button>
 </div>
