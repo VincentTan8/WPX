c1d8047bff9b0e56529b8c2ad7cd43b4e9a7b257
VincentTan8 <vincent.tan@wetalk.com>
2025-05-07 10:08:36 +0800

    fix: removed flag icons

--- index/translate-button.php
+++ index/translate-button.php
@@ -1,10 +1,8 @@
 <div id="language-options">
     <button data-lang="en">
-        <img src="../resources/img/en.png" alt="English Flag" style="width: 20px; height: 20px; margin-right: 8px;">
-        EN
+        English
     </button>
     <button data-lang="cn">
-        <img src="../resources/img/cn.png" alt="Chinese Flag" style="width: 20px; height: 20px; margin-right: 8px;">
         中文
     </button>
 </div>
