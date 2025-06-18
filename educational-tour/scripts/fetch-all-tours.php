1b5a341a8eb134239cbd49793218c4646b17d3bb
VincentTan8 <vincent.tan@wetalk.com>
2025-06-18 11:25:40 +0800

    feat: added fetch all available tours query logic

---
+++ educational-tour/scripts/fetch-all-tours.php
@@ -0,0 +1,23 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+    ob_start();
+}
+?>
+
+<?php
+include "../../connections/dbname.php";
+
+$tablename = $database . ".`wt_eductour_tour_details`";
+$sql = "SELECT `ref_num`, `destination_ref_num` FROM $tablename";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$tour_list = [];
+while ($row = $result->fetch_assoc()) {
+    $tour_list[] = $row;
+}
+echo json_encode($tour_list);
+?>
