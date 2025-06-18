baeec707aa63f1912aa8132f438b91c07c598a7c
VincentTan8 <vincent.tan@wetalk.com>
2025-06-18 11:09:37 +0800

    feat: added fetch program details query logic

---
+++ educational-tour/scripts/fetch-program-details.php
@@ -0,0 +1,31 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+    ob_start();
+}
+?>
+
+<?php
+include "../../connections/dbname.php";
+
+$tour_details_ref_num = $_POST['tour_details_ref_num']; // tour details ref_num
+$language = $_POST['language'];  //_en, _cn, _kr, _jp
+
+$title = "title" . $language;
+$description = "description" . $language;
+
+$tablename = $database . ".`wt_eductour_program_details`";
+$sql = "SELECT `program_img`, `$title` AS `title`, `$description` AS `description` FROM $tablename
+        WHERE `ref_num` = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $tour_details_ref_num);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$prog_det = [];
+while ($row = $result->fetch_assoc()) {
+    $prog_det[] = $row;
+}
+echo json_encode($prog_det);
+?>
