2077151cff8f968e3162d04735a85935942496b2
VincentTan8 <vincent.tan@wetalk.com>
2025-06-24 10:04:40 +0800

    chore: extra space deletion

--- dest/course_introduction.php
+++ dest/course_introduction.php
@@ -1,26 +1,16 @@
 <section class="bg-white" id="deal">
     <div class="container" style="margin-top: 2em;">
-
-
-
-
         <div class="col-md-12 text-left wow " style="margin-top: -400px;">
             <?php include "why.php" ?>
         </div>
-
-
-
 
         <div class="col-md-12 text-center wow ">
             <?php include "program.php" ?>
         </div>
 
-
-
         <div class="col-md-12 text-center wow ">
             <?php include "itinerary.php" ?>
         </div>
-
 
         <div class="col-md-12 text-center wow ">
             <?php include "accommodation.php" ?>
@@ -30,7 +20,6 @@
             <?php include "kids.php" ?>
         </div>
 
-
         <div class="col-md-12 text-center wow ">
             <?php include "journey.php" ?>
         </div>
@@ -38,6 +27,4 @@
         <div class="col-md-12 text-center wow " style="margin-top: 100px;">
             <?php include "next.php" ?>
         </div>
-
-
 </section>
