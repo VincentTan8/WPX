86b976f7d087e8a7a911e638b25a95ea7a0eeb65
Akatsv <jeff.uy@oseamatrix.com>
2025-07-01 14:08:44 +0800

    feat: updated educational tour header and dropdown

--- includes/header.php
+++ includes/header.php
@@ -93,13 +93,18 @@ $hide_panda = (
     $m_culture = 'Chinese Culture Learning';
     $m_teachers = 'Teachers';
     $m_news = 'News';
-    $m_study_abroad = 'Study Abroad';
+    $m_educational_tours = 'Educational Tours';
     $m_company = 'Company';
     $m_charity = 'Charity';
     $m_about_us = 'About Us';
     $m_campus = 'Campus';
     $m_sign_in = 'Sign In';
     $m_sigun_up = 'Sign Up';
+    $m_taiwan = 'Taiwan';
+    $m_china = 'China';
+    $m_singapore = 'Singapore';
+    $m_japan = 'Japan';
+    $m_philippines = 'Philippines';
 
 
 
@@ -114,13 +119,18 @@ $hide_panda = (
         $m_culture = '国学';
         $m_teachers = '进贤进能';
         $m_news = '温故知新';
-        $m_study_abroad = '留学';
+        $m_educational_tours = '游学团';
         $m_company = '公司介绍';
         $m_charity = '公益慈善';
         $m_about_us = '关于我们';
         $m_campus = '校区介绍';
         $m_sign_in = '登录';
         $m_sigun_up = '注册';
+        $m_taiwan = '台湾';
+        $m_china = '中国';
+        $m_singapore = '新加坡';
+        $m_japan = '日本';
+        $m_philippines = '菲律宾';
 
 
     }
@@ -274,9 +284,18 @@ $hide_panda = (
 
                     </ul>
                     <ul class="nav navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link link  <?php echo $menu_bar6 ?>"
-                                href="../study-abroad/"><?php echo $m_study_abroad ?></a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle   <?php echo $menu_bar6 ?>" href="../educational-tour/"
+                                id="navbarDropdown2" role="button">
+                                <?php echo $m_educational_tours ?>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                                <a class="dropdown-item" href="../tour/?dest=TW"><?php echo $m_taiwan ?></a>
+                                <a class="dropdown-item" href="../tour/?dest=CN"><?php echo $m_china ?></a>
+                                <a class="dropdown-item" href="../tour/?dest=SG"><?php echo $m_singapore ?></a>
+                                <a class="dropdown-item" href="../tour/?dest=JP"><?php echo $m_japan ?></a>
+                                <a class="dropdown-item" href="../tour/?dest=PH"><?php echo $m_philippines ?></a>
+                            </div>
                         </li>
 
 
