f3180877e735020847e086d6b9ef6e213193ffb8
Akatsv <jeff.uy@oseamatrix.com>
2025-07-01 13:18:20 +0800

    feat:updated and reverted back the id to hero-button-1 and hero-button-2

--- educational-tour/index.php
+++ educational-tour/index.php
@@ -198,7 +198,7 @@ $imgDir = "../resources/img/educational-tour/";
     <!-- Hero Buttons -->
     <div class="hero-buttons">
         <a href="#carousel-anchor" class="hero-button green">
-            <span id="hero-button-destination">Explore Our Destination</span>
+            <span id="hero-button-1">Explore Our Destination</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none">
                 <path d="M14 1L21 8M21 8L14 15M21 8H1" stroke="white" stroke-width="2" stroke-linecap="round"
                     stroke-linejoin="round" />
@@ -206,7 +206,7 @@ $imgDir = "../resources/img/educational-tour/";
         </a>
 
         <a href="your-journey-video-url" class="hero-button white">
-            <span id="watch-journey">Watch Our Journey</span>
+            <span id="hero-button-2">Watch Our Journey</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="black">
                 <path d="M8 5v14l11-7z" />
             </svg>
