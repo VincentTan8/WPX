a150a305ed9f03227d245be6fc394ce8bca0b44f
VincentTan8 <vincent.tan@wetalk.com>
2025-07-15 13:06:57 +0800

    fix: removed excess fields for add course

--- course/scripts/add-course.php
+++ course/scripts/add-course.php
@@ -11,82 +11,21 @@ if (!$conn) {
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $course_title_en = $_POST['addCourseTitleEN'];
-    $course_title_cn = $_POST['addCourseTitleCN'];
 
-    $course_short_title_en = $_POST['addCourseShortTitleEN'];
-    $course_short_title_cn = $_POST['addCourseShortTitleCN'];
-
-    $course_subtitle_en = $_POST['addCourseSubtitleEN'];
-    $course_subtitle_cn = $_POST['addCourseSubtitleCN'];
-
-    $course_description_en = $_POST['addCourseDescriptionEN'];
-    $course_description_cn = $_POST['addCourseDescriptionCN'];
-
-    $thumbnail_tag_en = $_POST['addThumbnailTagEN'];
-    $thumbnail_tag_cn = $_POST['addThumbnailTagCN'];
-
-    $suitable_for_en = $_POST['addSuitableForEN'];
-    $suitable_for_cn = $_POST['addSuitableForCN'];
-
-    $course_start_date_en = $_POST['addCourseStartDateEN'];
-    $course_start_date_cn = $_POST['addCourseStartDateCN'];
-
-    $class_hours_en = $_POST['addClassHoursEN'];
-    $class_hours_cn = $_POST['addClassHoursCN'];
-
-    $age_group = $_POST['addAgeGroup'];
-    $language = $_POST['addLanguage'];
-    $course_package = $_POST['addCoursePackage'];
-    $course_type = $_POST['addCourseType'];
-
-
-    $coursestable = $database . ".`wt_courses`";
-    $activitiestable = $database . ".`wt_course_activities`";
-    $featurestable = $database . ".`wt_course_features`";
-    $learninggoalstable = $database . ".`wt_course_learning_goals`";
-    $materialstable = $database . ".`wt_course_materials`";
-    $objectivestable = $database . ".`wt_course_objectives`";
-    $teacherstable = $database . ".`wt_course_teachers`";
+    $tablename = $database . ".`wt_courses`";
 
     //WeTalk Courses reference num generate
     $ref_num_prefix = 'WTC-';
-    $new_ref_num = generateRefNum($conn, $ref_num_prefix, $coursestable);
+    $new_ref_num = generateRefNum($conn, $ref_num_prefix, $tablename);
 
-    $sql = "INSERT INTO $coursestable (`ref_num`, `course_title_en`, `course_title_cn`,
-                `course_short_title_en`, `course_short_title_cn`,
-                `course_subtitle_en`, `course_subtitle_cn`,
-                `course_description_en`, `course_description_cn`,
-                `thumbnail_tag_en`, `thumbnail_tag_cn`,
-                `suitable_for_en`, `suitable_for_cn`,
-                `course_start_date_en`, `course_start_date_cn`,
-                `class_hours_en`, `class_hours_cn`,
-                `age_group`, `language`, `course_package`, `course_type`) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO $tablename (`ref_num`, `course_title_en`) 
+            VALUES (?, ?)";
 
     $stmt = $conn->prepare($sql);
     $stmt->bind_param(
-        "sssssssssssssssssssss",
+        "ss",
         $new_ref_num,
-        $course_title_en,
-        $course_title_cn,
-        $course_short_title_en,
-        $course_short_title_cn,
-        $course_subtitle_en,
-        $course_subtitle_cn,
-        $course_description_en,
-        $course_description_cn,
-        $thumbnail_tag_en,
-        $thumbnail_tag_cn,
-        $suitable_for_en,
-        $suitable_for_cn,
-        $course_start_date_en,
-        $course_start_date_cn,
-        $class_hours_en,
-        $class_hours_cn,
-        $age_group,
-        $language,
-        $course_package,
-        $course_type
+        $course_title_en
     );
 
     if ($stmt->execute()) {
