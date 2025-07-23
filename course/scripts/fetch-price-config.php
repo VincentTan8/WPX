44c26342c29de7c5dfcc8f7ad53879718b54db2c
VincentTan8 <vincent.tan@wetalk.com>
2025-07-23 10:16:09 +0800

    feat: added fetch files for price config and prices

---
+++ course/scripts/fetch-price-config.php
@@ -0,0 +1,48 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+    ob_start();
+}
+?>
+
+<?php
+//Fetch available selections for config
+include "../../connections/dbname.php";
+
+$age_group = $_POST['age_group'];
+$course_type = $_POST['course_type'];
+
+$tablename = $database . ".`wt_rates_prices`";
+$sessiontable = $database . ".`wt_rates_session_types`";
+
+$sql = "SELECT DISTINCT(rst.ref_num), rst.display_name FROM $sessiontable rst
+        JOIN $tablename t ON t.session_type_ref_num = rst.ref_num
+        WHERE t.course_category = ? AND t.age_group = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $course_type, $age_group);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$session_types = [];
+while ($row = $result->fetch_assoc()) {
+    $session_types[] = $row;
+}
+
+$sql = "SELECT DISTINCT(`num_sessions`) FROM $tablename
+        WHERE `course_category` = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $course_type);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$num_sessions = [];
+while ($row = $result->fetch_assoc()) {
+    $num_sessions[] = $row;
+}
+
+$config['session_types'] = $session_types;
+$config['num_sessions'] = $num_sessions;
+echo json_encode($config);
+?>
