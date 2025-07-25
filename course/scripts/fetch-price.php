9de17580909c409d0d7396e118f326dbe5a58578
VincentTan8 <vincent.tan@wetalk.com>
2025-07-25 17:19:10 +0800

    feat: added currency parameter in fetching price

--- course/scripts/fetch-price.php
+++ course/scripts/fetch-price.php
@@ -13,15 +13,17 @@ $age_group = $_POST['age_group'];
 $course_type = $_POST['course_type'];
 $session_type = $_POST['session_type_ref_num'];
 $num_sessions = $_POST['num_sessions'];
+$currency = $_POST['currency'];
 
 $tablename = $database . ".`wt_rates_prices`";
 
 $sql = "SELECT * FROM $tablename
         WHERE `course_category` = ? AND `age_group` = ? AND
-              `session_type_ref_num` = ? AND `num_sessions` = ?";
+              `session_type_ref_num` = ? AND `num_sessions` = ? AND
+              `currency` = ?";
 
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sssi", $course_type, $age_group, $session_type, $num_sessions);
+$stmt->bind_param("sssis", $course_type, $age_group, $session_type, $num_sessions, $currency);
 $stmt->execute();
 $result = $stmt->get_result();
 
