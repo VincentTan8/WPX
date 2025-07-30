f6a98e3b643ce391a583b11ce0455c976c4346e4
VincentTan8 <vincent.tan@wetalk.com>
2025-07-30 13:26:00 +0800

    feat: initial teachers backup

---
+++ teacher/approaches.php
@@ -0,0 +1,19 @@
+
+
+    <!--Mega Deal-->
+    <section class="bg-white" id="deal"  >
+        <div class="container" ">
+        
+
+<div class="col-md-12 text-center wow slideInUp"   >
+<picture >
+    <source srcset="../science/img/approaches_mobile.png" media="(max-width: 768px)">
+    <source   srcset="../science/img/approaches.png" media="(min-width: 769px)">
+    <img src="../science/img/approaches.png"  id="specific-image-full" alt="Responsive Image">
+  </picture>
+  </div>
+
+   </div>
+
+    </section>
+    <!--End Mega Deal-->
