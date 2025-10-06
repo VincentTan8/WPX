517ae304fc112a8381c4189d6a14c015346ecbc2
VincentTan8 <vincent.tan@wetalk.com>
2025-10-06 11:31:48 +0800

    feat: added botpress widget on main-page

--- index/main-page.php
+++ index/main-page.php
@@ -1553,6 +1553,10 @@ if (isset($_SESSION['lang']) and $_SESSION['lang'] == 'CN') {
     <script type="module" src="scripts/translate.js"></script>
     <script type="text/javascript" src="assets/warning.js"></script>
     <script type="text/javascript" src="scripts/hamburger.js"></script>
+
+    <!-- Botpress Script -->
+    <script src="https://cdn.botpress.cloud/webchat/v3.2/inject.js" defer></script>
+    <script src="https://files.bpcontent.cloud/2025/09/24/08/20250924084540-B5Q8A59C.js" defer></script>
 </body>
 
 </html>
