e81be53f71d95b7471b5af6b6b6e1418a4749ad6
VincentTan8 <vincent.tan@wetalk.com>
2025-12-04 13:36:03 +0800

    feat: added checker of payment intent id, added payment info display, implement script for retrieving payment status and status responses

--- pay/success.php
+++ pay/success.php
@@ -11,15 +11,19 @@ if (isset($_SESSION['lang']) and $_SESSION['lang'] == 'CN') {
     $lang = '_en';
 }
 
+//Handle Intent
+//https://wetalk.com/testpay/success.php?id=int_hkdmkkjbkhdin0qs1vi&type=SUCCESS_URL
+if (!isset($_GET['id'])) {
+    die('No payment intent ID provided.');
+}
+$intent_id = $_GET['id'];
+
 include "../includes/menu_bar_reset.php";
-$menu_bar4 = "active";
 
 $_SESSION['active_page'] = 'pay';
 
 include "../includes/header.php";
 include "../connections/dbname.php";
-
-
 ?>
 
 <!DOCTYPE html>
@@ -50,14 +54,95 @@ include "../connections/dbname.php";
 
 
 <body>
+    <div class="payment-form-body position-relative" data-spy="scroll" data-target=".navbar" data-offset="90">
+        <!-- <h1>WeTalk Unified Payment Page</h1> -->
+        <div class="payment-info">
+            <p class="success-status">
+                <span id="paymentStatus">Loading...</span>
+                <br><br>
+                <span id="paymentStatusSubtitle">Thank you for your purchase.</span>
+            </p>
+            <p class="success-order">Order ID:
+                <span id="orderID">Loading...</span>
+            </p>
+            <p class="success-course">Course:
+                <span id="coursePaid">Loading...</span>
+            </p>
+            <p class="success-paid">Amount Paid:
+                <span id="amountPaid">Loading...</span>
+            </p>
 
-
-    <?php include "../includes/booking.php" ?>
+            <!-- <p style="margin-top: 70px; margin-bottom: 10px">Need Assistance?</p>
+            <strong>Please Contact Support</strong>
+            <br>
+            <span>Singapore: +65 9758 - 2288</span>
+            <br>
+            <span>Philippines: +63 9171 – 6816 - 80</span>
+            <br>
+            <span>China: +86 1816 – 5370 - 116</span> -->
+        </div>
+    </div>
 
     <?php include "../includes/address.php" ?>
 
     <?php include "../includes/footer.php"; ?>
     <div id="page-data" data-page="pay" data-lang="<?php echo $lang; ?>"></div>
+
+    <script async>
+        (async () => {
+            const intent_id = <?php echo json_encode($intent_id); ?>;
+            const res = await fetch(`retrieve-payment-intent.php?intent_id=${intent_id}`, {
+                headers: { 'Content-Type': 'application/json' },
+            });
+            const data = await res.json();
+
+            if (data.success) {
+                const toPay = data.body.amount;
+                const amount = data.body.captured_amount;
+                const currency = data.body.currency;
+                const status = data.body.status;
+                const orderID = data.body.merchant_order_id;
+                const coursePaid = data.body.metadata?.course_name ?? 'N/A';
+
+                // document.getElementById('amountToPay').textContent = `${currency} ${toPay}`;
+                document.getElementById('coursePaid').textContent = `${coursePaid}`;
+                document.getElementById('amountPaid').textContent = `${currency} ${amount}`;
+                document.getElementById('orderID').textContent = `${orderID}`;
+
+                const statusSpan = document.getElementById('paymentStatus');
+                const statusSubSpan = document.getElementById('paymentStatusSubtitle');
+                switch (status) {
+                    case 'SUCCEEDED':
+                        statusSpan.textContent = 'Payment Successful!';
+                        break;
+                    case 'PENDING':
+                    case 'PENDING_REVIEW':
+                        statusSpan.textContent = 'Payment Pending';
+                        statusSpan.className = 'status-pending';
+                        statusSubSpan.textContent = 'Please refresh the page later';
+                        break;
+                    case 'CANCELLED':
+                        statusSpan.textContent = 'Payment Cancelled';
+                        statusSpan.className = 'status-failed';
+                        statusSubSpan.textContent = 'Cancellation Successful';
+                        break;
+                    case 'REQUIRES_PAYMENT_METHOD':
+                        statusSpan.textContent = 'Payment Failed';
+                        statusSpan.className = 'status-failed';
+                        statusSubSpan.textContent = 'Please try again';
+                        break;
+                    default:
+                        statusSpan.textContent = status;
+                        statusSpan.className = 'status-failed';
+                        statusSubSpan.textContent = 'Please try again';
+                }
+            } else {
+                console.error("Error:", data.error);
+                document.getElementById('amountPaid').textContent = '-';
+                document.getElementById('paymentStatus').textContent = 'Error';
+            }
+        })();
+    </script>
 </body>
 
 </html>
