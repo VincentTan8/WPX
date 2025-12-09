cf424c4c778f69d5547dc8eeecfbfec0970e9311
VincentTan8 <vincent.tan@wetalk.com>
2025-12-09 10:29:49 +0800

    refactor: converted payment success page into an email viewable version

--- pay/send-email.php
+++ pay/send-email.php
@@ -42,24 +42,93 @@ $mail->addAddress($email);
 $mail->isHTML(true);
 $subject = "WeTalk - Payment Successful";
 
-$bodyContent = "<div style='font-size: 16px'>
-    <p>Thank you for your payment, $full_name</p>
-    <p style='margin-left: 15px'><strong>Order ID:</strong> $order_id</p>
-    <p style='margin-left: 15px'><strong>Course:</strong> $course_name</p>
-    <p style='margin-left: 15px'><strong>Amount Paid:</strong> $currency $amount</p>
-    
-    <p style='margin-top: 20px; margin-bottom: 10px'>For assistance, please contact WeTalk Support. Details below.</p>
-    <span>Singapore: +65 9758 - 2288</span>
-    <br>
-    <span>Philippines: +63 9171 - 6816 - 80</span>
-    <br>
-    <span>China: +86 1816 - 5370 - 116</span>
-    </div>
+// $bodyContent = "<div style='font-size: 16px'>
+//     <p>Thank you for your payment, $full_name</p>
+//     <p style='margin-left: 15px'><strong>Order ID:</strong> $order_id</p>
+//     <p style='margin-left: 15px'><strong>Course:</strong> $course_name</p>
+//     <p style='margin-left: 15px'><strong>Amount Paid:</strong> $currency $amount</p>
 
-    <br><br>
-";
+//     <p style='margin-top: 20px; margin-bottom: 10px'>For assistance, please contact WeTalk Support. Details below.</p>
+//     <span>Singapore: +65 9758 - 2288</span>
+//     <br>
+//     <span>Philippines: +63 9171 - 6816 - 80</span>
+//     <br>
+//     <span>China: +86 1816 - 5370 - 116</span>
+//     </div>
+
+//     <br><br>
+// ";
+
+$bodyContent = '
+<table width="100%" cellpadding="0" cellspacing="0" style="background:#f4f4f4;padding:30px 0;">
+  <tr>
+    <td align="center">
+
+      <!-- Receipt Card -->
+      <table width="560" cellpadding="0" cellspacing="0" style="background:#ffffff;border-radius:12px;padding:40px 30px;font-family:Arial,Helvetica,sans-serif;text-align:center;">
+
+        <!-- Icon -->
+        <tr>
+          <td style="padding-bottom:25px;">
+            <img src="https://wetalk.com/testpay/assets/payment-icon.png" width="100" alt="Success" style="display:block;margin:auto;">
+          </td>
+        </tr>
+
+        <!-- Title -->
+        <tr>
+          <td style="font-size:35px;font-weight:bold;color:#ffb100;padding-bottom:6px;">
+            Payment Successful!
+          </td>
+        </tr>
+
+        <!-- Subtitle -->
+        <tr>
+          <td style="font-size:17px;color:#666;padding-bottom:25px;">
+            Your payment has been processed.
+          </td>
+        </tr>
+
+        <!-- Details -->
+        <tr>
+          <td align="center">
+            <table cellpadding="5" cellspacing="0" style="font-size:18px;color:#333;text-align:left;line-height:1.8;">
+              <tr>
+                <td style="padding-right:10px;"><strong>Order ID:</strong></td>
+                <td>' . $order_id . '</td>
+              </tr>
+              <tr>
+                <td><strong>Course:</strong></td>
+                <td>' . $course_name . '</td>
+              </tr>
+              <tr>
+                <td><strong>Amount Paid:</strong></td>
+                <td>' . $currency . ' ' . $amount . '</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+        <!-- Support Text -->
+        <tr>
+          <td style="font-size:15px;color:#777;padding-top:35px;line-height:1.6;">
+            For assistance, please contact WeTalk Support. Details below.<br><br>
+
+            <strong>Singapore:</strong> +65 9758-2288<br>
+            <strong>Philippines:</strong> +63 9171-6816-80<br>
+            <strong>China:</strong> +86 1816-5370-116
+          </td>
+        </tr>
+
+      </table>
+
+    </td>
+  </tr>
+</table>
+';
 $mail->Subject = $subject;
 $mail->Body = $bodyContent;
-$mail->send();
+if (!$mail->send()) {
+    echo $mail->ErrorInfo;
+}
 $mail->clearAddresses();
 $mail->clearAttachments();
