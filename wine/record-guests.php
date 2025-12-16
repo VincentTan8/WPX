c70b99d56d0aa7aa5aa031fec5f2315c9350cdae
VincentTan8 <vincent.tan@wetalk.com>
2025-12-16 17:13:47 +0800

    feat: changed intent parameter, added referral field, added order id updating, converted sql into prepared statements

--- wine/record-guests.php
+++ wine/record-guests.php
@@ -13,16 +13,34 @@ include "../connections/dbname.php";
 
 $input = json_decode(file_get_contents("php://input"), true);
 
-$intent_id = $input["intent"] ?? null;
+$intent_id = $input["intent_id"] ?? null;
 $guests = $input["guests"] ?? null;
+$referred_by = $input["referred_by"] ?? null;
+$order_id = $input["orderID"] ?? null;
 
 $tablename = $database . ".`wt_wine_guest`";
 
-foreach ($guests as $guest) {
-    $sql = "INSERT INTO $tablename (`intent_id`, `full_name`) 
-            VALUES ('$intent_id', '$guest');";
+if (!$order_id) {
+    $sql = "INSERT INTO $tablename (`intent_id`, `full_name`, `referred_by`) 
+            VALUES (?, ?, ?);";
+    $stmt = $conn->prepare($sql);
 
-    if (!$conn->query($sql)) {
-        echo "Guest [$guest] not saved";
+    foreach ($guests as $guest) {
+        $stmt->bind_param("sss", $intent_id, $guest, $referred_by);
+        if (!$stmt->execute()) {
+            echo "Guest [$guest] not saved";
+        }
+    }
+
+    $stmt->close();
+} else {
+    $sql = "UPDATE $tablename
+            SET `order_id` = ?
+            WHERE `intent_id` = ?;";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $order_id, $intent_id);
+
+    if (!$stmt->execute()) {
+        echo "Order ID [$order_id] not updated";
     }
 }
