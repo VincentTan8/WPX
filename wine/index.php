0ca3135c787a587a5446f74fd9e6d17674d462da
VincentTan8 <vincent.tan@wetalk.com>
2025-12-16 17:12:07 +0800

    feat: added fields and checkboxes, added script for airwallex payment, modified parameters for info requirements, added guest list script, added fetch call for recording guests

--- wine/index.php
+++ wine/index.php
@@ -72,13 +72,13 @@ include "../connections/dbname.php";
             <div class="event-info">
                 <p><strong>Event Details:</strong></p>
                 <p><strong>Date:</strong> Sunday, 21 December 2025</p>
-                <p><strong>Time:</strong> 4:00PM – 6:00PM</p>
+                <p><strong>Time:</strong> 4:00PM - 6:00PM</p>
                 <p><strong>Venue:</strong> WeTalk @ Delfi Orchard (#02-23/24)</p>
                 <p><strong>Fee:</strong> $128 per guest</p>
             </div>
 
             <p>
-                A <strong>free kids’ activity class</strong> will be available next door,
+                A <strong>free kids' activity class</strong> will be available next door,
                 so parents can enjoy the evening with ease while children engage in a fun creative session.
             </p>
 
@@ -105,12 +105,46 @@ include "../connections/dbname.php";
                 <label for="kidNumber">How many kids are you bringing?</label>
                 <input type="number" step="1" min="0" id="kidNumber" name="kidNumber" required>
 
-                <label for="guests">Add Guests</label>
+                <label for="dietary">Dietary Restrictions (if any)</label>
+                <input type="text" id="dietary" name="dietary">
+
+                <label for="guests">Additional Guests (if any)</label>
                 <div id="guestInput">
                     <div class="guestEntry">
                         <input type="text" name="guests[]" />
                         <button type="button" id="addGuest">+</button>
                     </div>
+                </div>
+
+                <label for="referredBy">Referred By (if applicable)</label>
+                <input type="text" id="referredBy" name="referredBy">
+
+                <div style="margin: 16px 0;">
+                    <label style="align-items:center;">
+                        <span>
+                            <input type="checkbox" id="agreeTerms" style="margin: 0; width: auto" required />
+                            I understand my registration is confirmed only upon payment verification.
+                        </span>
+                    </label>
+                </div>
+                <div style="margin: 16px 0;">
+                    <label style="align-items:center;">
+                        <span>
+                            <input type="checkbox" id="agreeTerms" style="margin: 0; width: auto" required />
+                            I consent to the collection and use of photographs and/or video recordings taken during the
+                            workshop by WeTalk Singapore and its authorised personnel for documentation and marketing
+                            purposes across print, electronic, and social media platforms.
+                        </span>
+                    </label>
+                </div>
+                <div style="margin: 16px 0;">
+                    <label style="align-items:center;">
+                        <span>
+                            <input type="checkbox" id="agreeTerms" style="margin: 0; width: auto" required />
+                            I have read and agree to the <a style="text-decoration: underline !important"
+                                href="../privacy" target="_blank" rel="noopener noreferrer">WeTalk Payment Policy</a>
+                        </span>
+                    </label>
                 </div>
                 <button type="submit" id="hpp">Pay Now</button>
             </form>
@@ -123,7 +157,100 @@ include "../connections/dbname.php";
     <?php include "../includes/footer.php"; ?>
     <div id="page-data" data-page="wine" data-lang="<?php echo $lang; ?>"></div>
 
+    <script async>
+        (async () => {
+            async function createPayment(course_name, currency, email, full_name, mobile_number, guest_num = 0) {
+                const res = await fetch('../resources/payment/create-payment-intent.php', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        //possibly add same parameters for fetching price such as group Setup
+                        //make it optional since this could be used for seasonal courses
+                        course_name,
+                        currency,
+                        email,
+                        full_name,
+                        mobile_number,
+                        guest_num
+                    })
+                });
 
+                const data = await res.json();
+                console.log(data);
+
+                if (data.success) {
+                    const intent = data.body.id;
+                    const client_secret = data.body.client_secret;
+
+                    return { intent, client_secret };
+                } else {
+                    console.error("Error:", data.error);
+                }
+            }
+            const mode = 'payment';
+            const { payments } = await window.AirwallexComponentsSDK.init({
+                env: 'demo', // Setup which Airwallex env('staging' | 'demo' | 'prod') to integrate with
+                enabledElements: ['payments'],
+            })
+
+            const redirectHppForCheckout = (currency, intent_id, client_secret) => {
+                payments.redirectToCheckout({
+                    env: 'demo',  //todo change when going to prod
+                    mode: 'payment',
+                    currency,
+                    intent_id,
+                    client_secret,
+                    successUrl: 'https://wetalk.com/wine/success.php', // Must be an HTTPS site todo change in prod
+                    appearance: {
+                        mode: 'light',
+                        variables: {
+                            colorBrand: '#f2b33d',
+                        },
+                    },
+                    // methods: "auto,"
+                    methods: ['googlepay', 'applepay', 'card', 'alipaycn', 'gcash', 'grabpay', 'kakaopay', 'wechatpay'],
+                    logoUrl: "https://wetalk.com/resources/img/logo.png",
+                    // shopper_email: ,
+                    // shopper_name: ,
+                    // shopper_phone: ,
+                });
+            }
+
+            document.getElementById('checkoutForm').addEventListener('submit', async function (e) {
+                e.preventDefault();
+
+                const course_name = "Wisdom of the Vine";
+                const currency = "SGD";
+                const email = document.getElementById("email").value;
+                const full_name = document.getElementById("fullName").value;
+                const mobile_number = document.getElementById("mobileNumber").value;
+                const referred_by = document.getElementById("referredBy").value;
+
+                const guestInputs = document.querySelectorAll('input[name="guests[]"]');
+                const guests = [...guestInputs]
+                    .map(input => input.value.trim())
+                    .filter(Boolean);
+                const guest_num = guests.length;
+
+                const { intent, client_secret } = await createPayment(course_name, currency, email, full_name, mobile_number, guest_num);
+
+                //save guest list to db before redirect, host is not included here
+                await fetch('record-guests.php', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        intent_id: intent,
+                        guests,
+                        referred_by
+                    })
+                });
+
+                if (mode === 'payment') {
+                    redirectHppForCheckout(currency, intent, client_secret);
+                }
+            });
+        })();
+    </script>
     <script src="scripts/wine.js"></script>
 </body>
 
