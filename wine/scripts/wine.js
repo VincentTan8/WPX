ae2c754e2e0c9e2fe16c104e90eb7bc73319b258
VincentTan8 <vincent.tan@wetalk.com>
2025-12-16 11:35:37 +0800

    feat: added js for add guests functionality

---
+++ wine/scripts/wine.js
@@ -0,0 +1,19 @@
+const guestInput = document.getElementById("guestInput");
+const addGuestBtn = document.getElementById("addGuest");
+
+function addGuest() {
+    const wrapper = document.createElement("div");
+
+    wrapper.className = `guestEntry`;
+    wrapper.innerHTML = `
+        <input type="text" name="guests[]" required>
+        <button type="button" onclick="this.parentNode.remove()">x</button>
+    `;
+
+    // <button type="button" onclick="this.parentNode.remove()">✖</button>
+    guestInput.appendChild(wrapper);
+}
+
+addGuestBtn.addEventListener("click", () => {
+    addGuest();
+});
