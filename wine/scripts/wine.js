786826c647adcd6f17a5a44fdfa44e8d88e4f16b
VincentTan8 <vincent.tan@wetalk.com>
2025-12-17 15:53:17 +0800

    refactor: update js script for adding new guest fields with indexing and removing them

--- wine/scripts/wine.js
+++ wine/scripts/wine.js
@@ -1,17 +1,22 @@
 const guestInput = document.getElementById("guestInput");
-const addGuestBtn = document.getElementById("addGuest");
+const addGuestBtn = document.getElementById("addGuestBtn");
+
+let guestIndex = 0;
 
 function addGuest() {
     const wrapper = document.createElement("div");
 
     wrapper.className = `guestEntry`;
     wrapper.innerHTML = `
-        <input type="text" name="guests[]">
-        <button type="button" onclick="this.parentNode.remove()">x</button>
+        <input type="text" name="guests[${guestIndex}][name]" placeholder="Name" required/>
+        <input type="email" name="guests[${guestIndex}][email]" placeholder="Email" required/>
+        <input type="tel" name="guests[${guestIndex}][mobile]" placeholder="Mobile number" required/>
+        <button type="button" class="guestBtn" onclick="this.parentNode.remove()">x</button>
     `;
 
     // <button type="button" onclick="this.parentNode.remove()">✖</button>
     guestInput.appendChild(wrapper);
+    guestIndex++;
 }
 
 addGuestBtn.addEventListener("click", () => {
