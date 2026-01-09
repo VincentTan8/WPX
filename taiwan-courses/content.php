d5662f5a06ad5a22615e7cc193ad38b3dc0deb93
VincentTan8 <vincent.tan@wetalk.com>
2026-01-09 08:48:06 +0800

    feat: added url builders for autofilling the payment page, converted the div elements to a elements for linking, added gallery section html structure

--- taiwan-courses/content.php
+++ taiwan-courses/content.php
@@ -1,3 +1,18 @@
+<?php
+$params = [
+    'course_name' => 'English Immersion Course',
+    'currency_code' => 'TWD'
+];
+
+$url1 = 'https://wetalk.com/pay?' . http_build_query($params);
+
+$params = [
+    'course_name' => 'Private English Course',
+    'currency_code' => 'TWD'
+];
+
+$url2 = 'https://wetalk.com/pay?' . http_build_query($params);
+?>
 <div class="rounded-container">
     <h1>Available Programs</h1>
 
@@ -23,7 +38,7 @@
         <p style="margin-bottom: 30px;">This programme is suitable for learners who wish to move beyond textbook
             learning and start using English more naturally.</p>
 
-        <div class="taiwan-btn">
+        <a class="taiwan-btn" href="<?= htmlspecialchars($url1) ?>">
             <span
                 style="font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 400; line-height: normal;">Proceed
                 to Pay</span>
@@ -32,7 +47,7 @@
                     d="M17.2929 0.292893C17.6834 -0.0976311 18.3166 -0.0976311 18.7071 0.292893L28.7071 10.2929C29.0976 10.6834 29.0976 11.3166 28.7071 11.7071L18.7071 21.7071C18.3166 22.0976 17.6834 22.0976 17.2929 21.7071C16.9024 21.3166 16.9024 20.6834 17.2929 20.2929L25.5858 12H1C0.447715 12 0 11.5523 0 11C0 10.4477 0.447715 10 1 10H25.5858L17.2929 1.70711C16.9024 1.31658 16.9024 0.683417 17.2929 0.292893Z"
                     fill="white" />
             </svg>
-        </div>
+        </a>
     </div>
 
     <div class="inner-container" style="margin-bottom: 100px;">
@@ -56,7 +71,7 @@
         <p style="margin-bottom: 30px;">This programme is ideal for students who prefer individual attention or have
             specific learning objectives.</p>
 
-        <div class="taiwan-btn">
+        <a class="taiwan-btn" href="<?= htmlspecialchars($url2) ?>">
             <span
                 style="font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 400; line-height: normal;">Proceed
                 to Pay</span>
@@ -65,8 +80,32 @@
                     d="M17.2929 0.292893C17.6834 -0.0976311 18.3166 -0.0976311 18.7071 0.292893L28.7071 10.2929C29.0976 10.6834 29.0976 11.3166 28.7071 11.7071L18.7071 21.7071C18.3166 22.0976 17.6834 22.0976 17.2929 21.7071C16.9024 21.3166 16.9024 20.6834 17.2929 20.2929L25.5858 12H1C0.447715 12 0 11.5523 0 11C0 10.4477 0.447715 10 1 10H25.5858L17.2929 1.70711C16.9024 1.31658 16.9024 0.683417 17.2929 0.292893Z"
                     fill="white" />
             </svg>
-        </div>
+        </a>
     </div>
 
     <h1>Classroom Photo Gallery</h1>
+
+    <div class="">
+        <div id="item-slider" class="owl-carousel owl-theme">
+            <div class="item">
+                <img class="gallery-img" src="assets/img-1.png">
+            </div>
+
+            <div class="item">
+                <img class="gallery-img" src="assets/img-2.png">
+            </div>
+
+            <div class="item">
+                <img class="gallery-img" src="assets/img-3.png">
+            </div>
+
+            <div class="item">
+                <img class="gallery-img" src="assets/img-4.png">
+            </div>
+
+            <div class="item">
+                <img class="gallery-img" src="assets/img-5.png">
+            </div>
+        </div>
+    </div>
 </div>
